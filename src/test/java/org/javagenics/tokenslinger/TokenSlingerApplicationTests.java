ca5353cb59358e2f032ba1f20f9fcffa42aa84b1
zanuah <kevincleveland@gmail.com>
2024-08-20 19:34:41 -0500

    Addiing test container support

--- src/test/java/org/javagenics/tokenslinger/TokenSlingerApplicationTests.java
+++ src/test/java/org/javagenics/tokenslinger/TokenSlingerApplicationTests.java
@@ -2,10 +2,18 @@ package org.javagenics.tokenslinger;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 class TokenSlingerApplicationTests {
 
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
     @Test
     void contextLoads() {
         // Not implement YET
