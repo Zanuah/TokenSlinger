5c5adf730a5db7d4ceb6311001eaeb0f3a85c38a
zanuah <kevincleveland@gmail.com>
2024-08-20 20:03:04 -0500

    Adding declaritive rest client.  The bean isn't being created.

--- src/main/java/org/javagenics/tokenslinger/TokenSlingerApplication.java
+++ src/main/java/org/javagenics/tokenslinger/TokenSlingerApplication.java
@@ -1,9 +1,11 @@
 package org.javagenics.tokenslinger;
 
+import io.github.danielliu1123.httpexchange.EnableExchangeClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableExchangeClients
 public class TokenSlingerApplication {
 
     public static void main(String[] args) {
--- src/test/java/org/javagenics/tokenslinger/TokenSlingerApplicationTests.java
+++ src/test/java/org/javagenics/tokenslinger/TokenSlingerApplicationTests.java
@@ -1,8 +1,13 @@
 package org.javagenics.tokenslinger;
 
+import org.javagenics.tokenslinger.model.Login;
+import org.javagenics.tokenslinger.restclient.TokenSlingerApi;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -11,11 +16,31 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Testcontainers
 class TokenSlingerApplicationTests {
 
+    @LocalServerPort
+    private int port;
+
+    private final TokenSlingerApi tokenSlingerApi;
+
+    public TokenSlingerApplicationTests(TokenSlingerApi tokenSlingerApi) {
+        this.tokenSlingerApi = tokenSlingerApi;
+    }
+
     @Container
     @ServiceConnection
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
+
     @Test
-    void contextLoads() {
+    void signinRestTest() {
+        tokenSlingerApi.login(new Login());
+    }
+
+    @Test
+    void protectedRouteRestTest() {
+        // Not implement YET
+    }
+
+    @Test
+    void loginRestTest() {
         // Not implement YET
     }
 }
